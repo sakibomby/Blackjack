71a9cd66d62f06ecd08211fdb263a1ceeca84ee5
sakibomby <csakae@gmail.com>
2024-04-02 08:45:14 -0700

    BJ

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
   /*----- constants -----*/
+const suits = ['s', 'c', 'd', 'h'];
+const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
+const dealerDeck = buildDealerDeck
 
 
   /*----- state variables -----*/
-
+let deck;
+let pHand; 
+let dHand;
+let pTotal; 
+let dTotal;
+let bankAmount;
+let bet;
+let outcome;
 
   /*----- cached elements  -----*/
 
@@ -11,3 +21,5 @@
 
 
   /*----- functions -----*/
+
+  
